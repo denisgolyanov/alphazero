acc5877b43655c0cccbbbc2c08ff5675551f7346
PZ Kaplan <pzkaplan@gmail.com>
2019-09-25 00:01:17 +0300

    bunch of stuff

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ from connect4.connect_four_training_spec import ConnectFourTrainingSpecification
 logger.info(f"Using cuda: {CUDA}")
 
 # train(TickTackToeTrainingSpecification())
-train(HtmfTrainingSpecification(), checkpoint="htmf2019-09-19T10_30_38_554978")
-#compete_with_user(ConnectFourTrainingSpecification(), "connect_four2019-09-06T14_47_05_079127")
+#train(HtmfTrainingSpecification(), checkpoint="htmf2019-09-19T10_30_38_554978")
+compete_with_user(HtmfTrainingSpecification(), "htmf2019-09-19T18_54_07_597763")
 
 
 # Complete TickTacToe model
